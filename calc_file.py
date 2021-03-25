edfed51b4ab63b2f219041341018696f93929b70
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-26 01:20:07 +0800

    calc t_beam i func done

--- calc_file.py
+++ calc_file.py
@@ -1,3 +1,15 @@
 #Remember that this file is strictly for just calculations, so tkinter stuff must be going on
 
+def calc_t_i(B, h, H, b):
+    centroid_numerator = ((H+(h/2))*(B*h))+((H/2)*(b*H))
+    centroid_denominator = ((B*h)+(b*H))
+    centroid = centroid_numerator/centroid_denominator
+    I1 = (1/12)*B*(h**3)
+    I2 = (1/12)*b*(H**3)
+    A1 = B*h
+    A2 = b*H
+    d1 = (H+(h/2)) - centroid
+    d2 = centroid - (H/2)
+    t_i = (I1 + (A1*(d1**2))) + (I2 + (A2*(d2**2)))
+    return t_i
 
